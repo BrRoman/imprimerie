7d6f02405083d826c4c8dd127cb2d72c3e18d23c
BrRoman <editeur@traditions-monastiques.com>
2021-11-17 04:10:23 +0100

    WorkForm: autofocus on name.

--- imprimerie/apps/main/forms.py
+++ imprimerie/apps/main/forms.py
@@ -4,12 +4,18 @@ from django import forms
 
 from .models import Work
 
+
 class WorkForm(forms.ModelForm):
     """ Form for Work. """
     name = forms.CharField(
         max_length=255,
         label='Nom',
         label_suffix='',
+        widget=forms.TextInput(
+            attrs={
+                'autofocus': 'autofocus',
+            }
+        )
     )
 
     class Meta:
