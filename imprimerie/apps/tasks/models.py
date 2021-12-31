28ff9318187b7dc7211a116432a10310e7ef2666
BrRoman <editeur@traditions-monastiques.com>
2021-12-31 21:42:06 +0100

    Tasks: model improved.

--- imprimerie/apps/tasks/models.py
+++ imprimerie/apps/tasks/models.py
@@ -1,12 +1,24 @@
 """ apps/tasks/models.py """
 
 from django.db import models
+from django.db.models.deletion import CASCADE
+
+from apps.clients.models import Client
+from apps.papers.models import Paper
 
 
 class Task(models.Model):
-    """ Task Model. """
+    """ Task model. """
+    client = models.ForeignKey(
+        Client,
+        on_delete=CASCADE,
+    )
     name = models.CharField(
         max_length=255,
+    )
+    paper = models.ForeignKey(
+        Paper,
+        on_delete=CASCADE,
     )
     created = models.DateTimeField(
         auto_now_add=True,
